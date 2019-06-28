a5d7de1ecb6f09425f504b9a520bd38ba25412e2
mairoot <maitoor@163.com>
2019-06-28 16:32:47 +0800

    elk

---
+++ dockerfile/elk/docker-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+_term() {
+  echo "Terminating ELK stoping"
+  kill -9 java
+  exit 0
+}
+trap _term SIGTERM SIGINT
+OUTPUT_LOGFILES=""
+ELK_VERSION="7.1"
+
+if [ ! $OUTPUT_LOGFILES ]; then
+cat << EOB
+    
+    ******************************************************
+    *                                                    *
+    *    Copyright: https://www.mairoot.com              *
+    *    Docker image: mairoot/elk                       *
+    *    https://github.com/mairoot/docker/elk           *
+    *                                                    *
+    ******************************************************
+    SERVER SETTINGS
+    ---------------
+    + elk version: $ELK_VERSION
+    + elk version: $ELK_VERSION
+    + elk version: $ELK_VERSION
+EOB
+fi
+
+ELK_HOME=/data/elk
+ES_HOME=$ELK_HOME/elasticsearch
+LGS_HOME=$ELK_HOME/logstash
+KBN_HOME=$ELK_HOME/kibana
+
+$ES_HOME/bin/elasticsearch -d
+OUTPUT_LOGFILES+="$ES_HOME/logs/elasticsearch.log "
+$KBN_HOME/bin/kibana >> $KBN_HOME/logs/kibana.log &
+OUTPUT_LOGFILES+="$KBN_HOME/logs/kibana.log "
+
+touch $OUTPUT_LOGFILES
+tail -f $OUTPUT_LOGFILES &
+wait
